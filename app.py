3f4c9cc82e680745f8e7d2d95b96f1e6dc26ce45
pokabu55 <okazumaru@gmail.com>
2022-12-31 15:19:53 +0900

    適当なGUI確認のサンプルコード

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.title("hello")
+st.title("hello world!")
+
+st.markdown("# Head1")
+st.write("* write")
+st.markdown("## Head2")
+st.write("* descripstion.")
+
+# サイドバー
+inputString = st.sidebar.text_input("text_input:ここに書くと引数に内容を渡せる")
+areaString = st.sidebar.text_area("text_area:何が違う？")
+if st.sidebar.button("commit"):
+    st.sidebar.write(inputString)
+    st.sidebar.write(areaString)
+
+
+
+check = st.checkbox("設定") # bool を返すんだって
+
+if check:
+    st.write("設定を隠したり")
