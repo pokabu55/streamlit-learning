d21348de07cf218fd8cc2314e9f9e92d58c26ebd
pokabu55 <okazumaru@gmail.com>
2023-02-25 14:17:56 +0900

    とりあえずライブ像でCanny

--- camera-app.py
+++ camera-app.py
@@ -6,4 +6,11 @@ import av #strealing video library
 st.title('Streamlit App Test')
 st.write('Hello world')
 
-webrtc_streamer(key='example')
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+webrtc_streamer(key='example', video_frame_callback=callback)
