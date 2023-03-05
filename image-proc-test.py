75ffdfabddb0d7a56eb6f4d30ad8c2711754c8bb
pokabu55 <okazumaru@gmail.com>
2023-03-05 23:46:56 +0900

    ネイティブアプリを使った処理のサンプル

--- image-proc-test.py
+++ image-proc-test.py
@@ -6,6 +6,28 @@ import cv2
 import tempfile
 from pathlib import Path
 
+import subprocess
+
+def image_processing(srcImage, processName):
+    # 画像処理のメイン関数的な
+
+    # 処理画像をネイティブで処理できるようにファイル出力
+    tmpSrcFileName = "./tmp/srcImg.png"
+    tmpDstFileName = "./tmp/dstImg.png"
+
+    # 何か処理してみるか
+    procImageCV = pil2cv(srcImage)
+    srcImage.save(tmpSrcFileName)
+    # エラー処理は？
+
+    # ネイティブアプリを呼び出す
+    subprocess.call("./modules/sample/bin/sample")
+
+    # 出力画像作成
+    dstImage = cv2.cvtColor(procImageCV, cv2.COLOR_BGR2RGB)
+
+    return dstImage
+
 def pil2cv(image):
     ''' PIL型 -> OpenCV型 '''
     new_image = np.array(image, dtype=np.uint8)
@@ -21,7 +43,7 @@ def main():
     uploaded_image = st.file_uploader("画像ファイルアップロード",type=['png', 'jpg', 'bmp'])
 
     # 縦並びでレイアウト
-    
+
     if uploaded_image is not None:
         # 画像ファイルを直接指定する場合
         #st.image(uploaded_image)
@@ -32,13 +54,8 @@ def main():
         # これは、選択したいね
         st.image(originalImage, caption="original image", use_column_width=True)
 
-        # 何か処理してみるか
-        procImageCV = pil2cv(originalImage)
-
         # 処理
-
-        # 表示用に戻す
-        dispImage = cv2.cvtColor(procImageCV, cv2.COLOR_BGR2RGB)
+        dispImage = image_processing(originalImage, "threthold")
 
         # 2個めの画像を開けば勝手に縦並びになっとる
         st.image(dispImage, caption="processed image", use_column_width=True)
